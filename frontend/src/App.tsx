95cf07fdeeb678fc827e43a6fdaf1bac51099737
jungjinbeom <insunok0715@naver.com>
2024-04-07 23:18:08 +0900

    선언 함수 분리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,13 +7,6 @@ import { routeTree } from "./routeTree.gen";
 // Create a new router instance
 const router = createRouter({ routeTree });
 
-// Register the router instance for type safety
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
-}
-
 const queryClient = new QueryClient();
 
 function App() {
